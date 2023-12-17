7ace0b0e223e4c47a8cf6d494d78a35c865969ef
Pudro <ptr.pustelnik@gmail.com>
2023-12-18 00:57:02 +0100

    Moved Args to separate file

--- training/adversarial_dqn.py
+++ training/adversarial_dqn.py
@@ -1,4 +1,3 @@
-# from magent2.environments import adversarial_pursuit_v4
 from environments import custom_adversarial
 from time import sleep
 import torch
@@ -8,67 +7,12 @@ from torch.distributions.categorical import Categorical
 import torch.nn.functional as F
 from stable_baselines3.common.buffers import ReplayBuffer
 import torch.optim as optim
-from dataclasses import dataclass
 import random
 
 # env = adversarial_pursuit_v4.parallel_env(map_size=10, render_mode='human')
-env = custom_adversarial.parallel_env(map_size=20, render_mode='human')
+env = custom_adversarial.parallel_env(map_size=32, render_mode='rgb_array')
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-@dataclass
-class Args:
-    # exp_name: str = os.path.basename(__file__)[: -len(".py")]
-    # """the name of this experiment"""
-    seed: int = 1
-    """seed of the experiment"""
-    torch_deterministic: bool = True
-    """if toggled, `torch.backends.cudnn.deterministic=False`"""
-    cuda: bool = True
-    """if toggled, cuda will be enabled by default"""
-    track: bool = False
-    """if toggled, this experiment will be tracked with Weights and Biases"""
-    wandb_project_name: str = "cleanRL"
-    """the wandb's project name"""
-    wandb_entity: str | None = None
-    """the entity (team) of wandb's project"""
-    capture_video: bool = False
-    """whether to capture videos of the agent performances (check out `videos` folder)"""
-    save_model: bool = False
-    """whether to save model into the `runs/{run_name}` folder"""
-    upload_model: bool = False
-    """whether to upload the saved model to huggingface"""
-    hf_entity: str = ""
-    """the user or org name of the model repository from the Hugging Face Hub"""
-
-    # Algorithm specific arguments
-    env_id: str = "CartPole-v1"
-    """the id of the environment"""
-    total_timesteps: int = 500000
-    """total timesteps of the experiments"""
-    learning_rate: float = 2.5e-4
-    """the learning rate of the optimizer"""
-    num_envs: int = 1
-    """the number of parallel game environments"""
-    buffer_size: int = 10000
-    """the replay memory buffer size"""
-    gamma: float = 0.99
-    """the discount factor gamma"""
-    tau: float = 1.0
-    """the target network update rate"""
-    target_network_frequency: int = 500
-    """the timesteps it takes to update the target network"""
-    batch_size: int = 128
-    """the batch size of sample from the reply memory"""
-    start_e: float = 1
-    """the starting epsilon for exploration"""
-    end_e: float = 0.05
-    """the ending epsilon for exploration"""
-    exploration_fraction: float = 0.5
-    """the fraction of `total-timesteps` it takes from start-e to go end-e"""
-    learning_starts: int = 10000
-    """timestep to start learning"""
-    train_frequency: int = 10
-    """the frequency of training"""
 
 class Training:
     def __init__(self) -> None:
