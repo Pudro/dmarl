9476c8156ae428dc9b42a65fec8dac2095e0586d
Piotr Pustelnik <ptr.pustelnik@gmail.com>
2024-04-26 15:31:49 +0200

    mfq experimantal layers

--- agents/mfq.py
+++ agents/mfq.py
@@ -1,4 +1,4 @@
-from numpy import who
+import numpy as np
 import torch
 from argparse import Namespace
 from typing import Optional, Union
@@ -33,33 +33,41 @@ class MFQ_Agent(Base_Agent):
             handle_timeout_termination=False,
         )
 
-        self.target_network = copy.deepcopy(self.network)
-        self.target_network.load_state_dict(self.network.state_dict())
-        self.target_network.to(self.device)
+        # self.target_network = copy.deepcopy(self.network)
+        # self.target_network.load_state_dict(self.network.state_dict())
+        # self.target_network.to(self.device)
 
         input_dim = self.env.action_spaces[self.agent_name].n
         hidden_dims = self.agent_config.hidden_mean_layers
         output_dim = self.env.action_spaces[self.agent_name].n
-        mean_network_layers = self._get_network_layers(input_dim, hidden_dims, output_dim)
-        self.mean_network = nn.Sequential(*mean_network_layers)
+        # mean_network_layers = self._get_network_layers(input_dim, hidden_dims, output_dim)
+        # self.mean_network = nn.Sequential(*mean_network_layers)
+        #
+        # self.target_mean_network = copy.deepcopy(self.mean_network)
+        # self.target_mean_network.load_state_dict(self.mean_network.state_dict())
+        # self.target_mean_network.to(self.device)
+        #
+        # self.cat_layer = nn.Linear(output_dim*2, output_dim)
 
-        self.target_mean_network = copy.deepcopy(self.mean_network)
-        self.target_mean_network.load_state_dict(self.mean_network.state_dict())
-        self.target_mean_network.to(self.device)
-
-        self.cat_layer = nn.Linear(output_dim*2, output_dim)
+        obs_dim = np.array(self.env.observation_spaces[self.agent_name].shape).prod()
+        network_layers = self._get_network_layers(input_dim + obs_dim, hidden_dims, output_dim)
+        self.network = nn.Sequential(*network_layers)
+        self.target_network = copy.deepcopy(self.network)
 
         self.optimizer = optim.Adam(self.parameters(), lr=self.agent_config.learning_rate, eps=1e-5)
         self.to(self.device)
 
     def forward(self, observations: torch.Tensor, actions_mean: torch.Tensor):
-        mean_values = self.mean_network(actions_mean)
-        agent_values = self.network(observations)
+        # mean_values = self.mean_network(actions_mean)
+        # agent_values = self.network(observations)
 
-        return self.cat_layer(torch.cat((agent_values, mean_values), dim=-1))
+        # return self.cat_layer(torch.cat((agent_values, mean_values), dim=-1))
+
+        return self.network(torch.cat((observations, actions_mean), dim=-1))
     
     def target(self, observations: torch.Tensor, actions_mean: torch.Tensor):
-        mean_values = self.target_mean_network(actions_mean)
-        agent_values = self.target_network(observations)
+        # mean_values = self.target_mean_network(actions_mean)
+        # agent_values = self.target_network(observations)
 
-        return self.cat_layer(torch.cat((agent_values, mean_values), dim=-1))
+        # return self.cat_layer(torch.cat((agent_values, mean_values), dim=-1))
+        return self.target_network(torch.cat((observations, actions_mean), dim=-1))
